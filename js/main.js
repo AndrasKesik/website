f3d45f91137aa08a2a4040c296dd378cc9ecb7db
zavox <andras.kesik@gmail.com>
2016-05-25 21:03:03 +0200

    javascript  working

--- js/main.js
+++ js/main.js
@@ -1,50 +1,47 @@
 /**
  * Created by kesikandras on 2016.05.21..
  */
-
-
-function endsWith(source, target) {
-    var start = source.length - target.length;
-    return start >= 0 && source.indexOf(target, start) == start;
-}
-
-var validateContactData = function(){
-    var validName = false;
-    var validEmail = false;
-    var validMessage = false;
+(function () {
     var name = document.getElementById("name");
     var email = document.getElementById("email");
     var message = document.getElementById("message");
-    var n = name.innerHTML;
-    var e = email.innerHTML;
-    var m = message.innerHTML;
-    if(name.length>3 && name.length<30) {
-        console.log(name);
-        validName = true;
-    }
-    if(endsWith(email,".hu") && endsWith(email,".com"))
-    {
-        console.log(email);
-        validEmail = true;
-    }
-    if(message.length>20 && message.length<200){
-        validMessage = true;
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
     }
 
-    if(validName && validEmail && validMessage){
-        document.getElementById("sendButton").disabled = false;
+    var validateContactData = function () {
+        var validName = false;
+        var validEmail = false;
+        var validMessage = false;
+
+        if (name.value.length > 3 && name.value.length < 30) {
+            console.log(name);
+            validName = true;
+        }
+        if (validateEmail(email.value)) {
+            console.log(email);
+            validEmail = true;
+        }
+        if (message.value.length > 20 && message.value.length < 200) {
+            validMessage = true;
+        }
+
+        if (validName && validEmail && validMessage) {
+            document.getElementById("sendButton").disabled = false;
+        }
+
+
+    };
+
+
+    var sendMessage = function () {
+
     }
 
 
-};
-
-
-var sendMessage = function(){
-
-}
-
-
-
-
-
-
+    name.addEventListener('change', validateContactData);
+    email.addEventListener('change', validateContactData);
+    message.addEventListener('change', validateContactData);
+})();
